55c1224920af52421dcb7afc342aef7f6726b0aa
iigmir <roc120j@gmail.com>
2020-07-29 23:42:19 +0800

    Add the other test files

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,29 @@
-const assert = require( "assert" );
 const FileSystemModule = require( "../FileSystemModule.js" );
 const app = new FileSystemModule();
 
-const case1 = async () =>
+const read_file_ttd = async () =>
 {
     const res = await app.read_file( "test/case1.txt" );
     const exp = res.includes( "Hello World!" ) === true;
     const msg = `The actual response is: ${ res }`;
     console.assert( exp, msg );
 };
-case1();
+read_file_ttd();
+
+const read_directory_ttd = async () =>
+{
+    const res = await app.read_directory( "test" );
+    const exp = res.includes( "index.spec.js" );
+    const msg = `The actual response is: ${ res }`;
+    console.assert( exp, msg );
+};
+read_directory_ttd();
+
+const read_json_ttd = async () =>
+{
+    const res = await app.read_json( "test/vaild.json" );
+    const exp = res.name === "Mitsuha Miyamizu";
+    const msg = `The actual response is: ${ res }`;
+    console.assert( exp, msg );
+};
+read_json_ttd();
